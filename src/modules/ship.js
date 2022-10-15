d5bd0b725cb32c6fbd8a15ddd224c758a5faf0f0
bst003 <bstanner33@yahoo.com>
2022-10-14 23:06:51 -0400

    Add coords property to Ship

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -3,11 +3,14 @@
 Need to add coordinates var (array) to Ship to store where the ship is placed
 
 */
-export const Ship = (shipLength) => {
+export const Ship = (shipLength, coords) => {
     let _hits = 0;
     const _length = shipLength;
+    const _coords = coords;
 
     const getHits = () => _hits;
+
+    const getCoords = () => _coords;
 
     const hit = () => {
         _hits++;
@@ -23,6 +26,7 @@ export const Ship = (shipLength) => {
     const testMethod = (a, b) => a + b;
 
     return {
+        getCoords,
         getHits,
         hit,
         isSunk,

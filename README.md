04b90700f83342500236245bf4f11f20c4a008d6
bst003 <bstanner33@yahoo.com>
2022-11-06 20:34:28 -0500

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# battleship
+# Battleship
+
+This project was completed as a way to experiment with
+Test Driven Development (TDD) and gain more experience with OOP. I used the Jest Testing Framework for this project.
+Using Jest I was able to implement almost all of the game logic
+before building anything in the DOM.
+
+The game works by setting up a GameLoop object and creating two players.
+Each player is then assigned a board object, which in turn is assigned five ship objects.
+The game then steps between turns until a player wins the game.
+
+<a href="#">View the live project</a>
--- src/index.js
+++ src/index.js
@@ -8,57 +8,5 @@ import { domFunctions } from "./modules/domStuff";
 import { reusedMethods } from "./modules/reusedMethods";
 import { pubsub } from "./modules/pubsub";
 
-/*
-
-TODO
-    ***Remove modal event listeners on remove
-
-    ***Remove board modals on ship placement
-
-    ***Remove board elements on game over
-
-    ***Add modal to replay game
-
-    ***Clear players list on game over
-
-    Update styling
-
-*/
-
 pubsub.publish("startGame");
-
-// const game = Gameloop();
-// game.addPlayer();
-// game.addPlayer(true);
-
-// const player1 = game.getPlayers()[0];
-// const player2 = game.getPlayers()[1];
-
-// window.game = game;
-// window.player1 = player1;
-// window.player2 = player2;
-
-// const player1Board = player1.getPlayerBoard();
-
-// player1Board.placeShip([4, 4], "vert", 5);
-// player1Board.placeShip([0, 3], "vert", 4);
-// player1Board.placeShip([2, 2], "hori", 3);
-// player1Board.placeShip([6, 9], "hori", 3);
-// player1Board.placeShip([0, 0], "hori", 2);
-
-// const player2Board = player2.getPlayerBoard();
-// player2Board.placeAllComputerShips();
-
-// player1.attack(player2, [9, 9]);
-
 MicroModal.init();
-
-// pubsub.publish("testModal");
-
-// player2.attack(player1, [9, 9]);
-// player2.attack(player1, [0, 0]);
-// player2.attack(player1, [1, 0]);
-// player2.attack(player1, [0, 3]);
-// player2.attack(player1, [0, 4]);
-// player2.attack(player1, [0, 5]);
-// player2.attack(player1, [0, 6]);
